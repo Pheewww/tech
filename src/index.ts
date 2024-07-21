78abdf8f8592fcd219d7fe98d636196b0a72fe97
Pheewww <umangsingh67899876@gmail.com>
2024-07-21 16:48:15 +0530

    prisma update data

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,47 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function insertUser(username: string, password: string, firsName: string, lastName: string) {
-  const res = await prisma.user.create({
-    data: {
-        username,
-        password,
-        firsName,
-        lastName
-    }
-  })
-  console.log(res);
+// async function insertUser(username: string, password: string, firsName: string, lastName: string) {
+//   const res = await prisma.user.create({
+//     data: {
+//         username,
+//         password,
+//         firsName,
+//         lastName
+//     }
+//   })
+//   console.log(res);
+// }
+
+// insertUser("admin1", "123456", "umang", "singh")
+
+
+
+interface UpdateParams {
+    firsName: string;
+    lastName: string;
 }
 
-insertUser("admin1", "123456", "umang", "singh")
+async function updateUser(username: string, {
+    firsName,
+    lastName
+}: UpdateParams) {
+
+  const yo  = await prisma.user.update({
+    where: {username}, 
+    data: {
+      firsName,
+      lastName
+    }
+  })
+
+  console.log(yo);
+
+  
+  
+}
+
+updateUser("admin1", {
+  firsName: "NEW",
+  lastName: "Singh"
+})
